b40a6bfe00ee9b8c291f1784d35363accc5a93a5
Sebastian May <sebastian.may@adesso.de>
2021-10-25 08:56:28 +0200

    uses ws_steam_wasm and async as poc. May need to find a better way for client to server communication.

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -18,6 +18,12 @@ use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WidgetExt, Wi
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{ErrorEvent, MessageEvent, WebSocket};
+
+use pharos::*;
+use wasm_bindgen::UnwrapThrowExt;
+use wasm_bindgen_futures::spawn_local;
+use ws_stream_wasm::*;
+
 const VERTICAL_WIDGET_SPACING: f64 = 20.0;
 const TEXT_BOX_WIDTH: f64 = 200.0;
 
@@ -110,77 +116,21 @@ extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
+
 fn login(login_state: &mut LoginState) {
-    let ws = WebSocket::new("ws://localhost:8000/ws/").expect("connection");
-    // For small binary messages, like CBOR, Arraybuffer is more efficient than Blob handling
-    ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
-    // create callback
-    let cloned_ws = ws.clone();
-    let onmessage_callback = Closure::wrap(Box::new(move |e: MessageEvent| {
-        // Handle difference Text/Binary,...
-        if let Ok(abuf) = e.data().dyn_into::<js_sys::ArrayBuffer>() {
-            console_log!("message event, received arraybuffer: {:?}", abuf);
-            let array = js_sys::Uint8Array::new(&abuf);
-            let len = array.byte_length() as usize;
-            console_log!("Arraybuffer received {}bytes: {:?}", len, array.to_vec());
-            // here you can for example use Serde Deserialize decode the message
-            // for demo purposes we switch back to Blob-type and send off another binary message
-            cloned_ws.set_binary_type(web_sys::BinaryType::Blob);
-            match cloned_ws.send_with_u8_array(&vec![5, 6, 7, 8]) {
-                Ok(_) => console_log!("binary message successfully sent"),
-                Err(err) => console_log!("error sending message: {:?}", err),
-            }
-        } else if let Ok(blob) = e.data().dyn_into::<web_sys::Blob>() {
-            console_log!("message event, received blob: {:?}", blob);
-            // better alternative to juggling with FileReader is to use https://crates.io/crates/gloo-file
-            let fr = web_sys::FileReader::new().unwrap();
-            let fr_c = fr.clone();
-            // create onLoadEnd callback
-            let onloadend_cb = Closure::wrap(Box::new(move |_e: web_sys::ProgressEvent| {
-                let array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
-                let len = array.byte_length() as usize;
-                console_log!("Blob received {}bytes: {:?}", len, array.to_vec());
-                // here you can for example use the received image/png data
-            })
-                as Box<dyn FnMut(web_sys::ProgressEvent)>);
-            fr.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
-            fr.read_as_array_buffer(&blob).expect("blob not readable");
-            onloadend_cb.forget();
-        } else if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
-            console_log!("message event, received Text: {:?}", txt);
-        } else {
-            console_log!("message event, received Unknown: {:?}", e.data());
-        }
-    }) as Box<dyn FnMut(MessageEvent)>);
-    // set message event handler on WebSocket
-    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
-    // forget the callback to keep it alive
-    onmessage_callback.forget();
+    spawn_local(async {
+        let (mut ws, _wsio) = WsMeta::connect("ws://127.0.0.1:8000/ws/", None)
+            .await
+            .expect_throw("assume the connection succeeds");
 
-    let onerror_callback = Closure::wrap(Box::new(move |e: ErrorEvent| {
-        console_log!("error event: {:?}", e);
-    }) as Box<dyn FnMut(ErrorEvent)>);
-    ws.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
-    onerror_callback.forget();
+        //        let evts = ws.observe(ObserveConfig::default()).expect_throw("bla");
 
-    let cloned_ws = ws.clone();
-    let onopen_callback = Closure::wrap(Box::new(move |_| {
-        console_log!("socket opened");
-        match cloned_ws.send_with_str("ping") {
-            Ok(_) => console_log!("message successfully sent"),
-            Err(err) => console_log!("error sending message: {:?}", err),
-        }
+        ws.close().await;
 
-        match cloned_ws.send_with_str("login u p") {
-            Ok(_) => console_log!("login successfully sent"),
-            Err(err) => console_log!("error sending message: {:?}", err),
-        }
-        // send off binary message
-        // match cloned_ws.send_with_u8_array(&vec![0, 1, 2, 3]) {
-        //     Ok(_) => console_log!("binary message successfully sent"),
-        //     Err(err) => console_log!("error sending message: {:?}", err),
-        // }
-    }) as Box<dyn FnMut(JsValue)>);
-    ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
-    onopen_callback.forget();
+        // Note that since WsMeta::connect resolves to an opened connection, we don't see
+        // any Open events here.
+        //
+        //assert!(evts.next().await.unwrap_throw().is_closing());
+        //assert!(evts.next().await.unwrap_throw().is_closed());
+    });
 }
