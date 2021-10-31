19ae7e4e26a1d468f9f743907ae9c66f955d08ab
Sebastian May <sebastian.may@adesso.de>
2021-10-31 11:04:32 +0100

    uses wasm deps when target is wasm

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,14 +1,14 @@
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 cfg_if::cfg_if! {
-    if #[cfg(feature = "wee_alloc")] {
+    if #[cfg(target_family = "wasm")] {
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
 }
 
 cfg_if::cfg_if! {
-    if #[cfg(feature = "console_log")] {
+    if #[cfg(target_family = "wasm")] {
         fn init_log() {
             use log::Level;
             console_log::init_with_level(Level::Trace).expect("error initializing log");
@@ -54,7 +54,7 @@ struct LoginState {
 pub fn wasm_main() {
     // This hook is necessary to get panic messages in the console
     cfg_if::cfg_if! {
-        if #[cfg(feature = "console_error_panic_hook")] {
+        if #[cfg(target_family = "wasm")] {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
         }
     }
